3e0c4d8ddd7adcf0f6fc86f510c8d233069f909a
Anna Brogowski <superg98@vt.edu>
2021-12-19 02:58:18 -0500

    updated sizing for correctness--need to also work on the scaling of enemies

--- main.py
+++ main.py
@@ -16,18 +16,18 @@ pygame.display.set_icon(icon)
 shipImg = pygame.image.load('warstarship.png')
 shipImg = pygame.transform.scale(shipImg, (1420, 1020))  # Scale it to the size of the window
 # X and Y positions for movement later
-shipImgX = -10
-shipImgY = -10
+shipImgX = -15
+shipImgY = -15
 
 # Enemy Ship image
 enemyImg = pygame.image.load('warstarenemy.png')
-enemyImg = pygame.transform.scale(enemyImg, (10, 10))  # Scale it to the size of the window
+enemyImg = pygame.transform.scale(enemyImg, (45, 10))  # Scale it to smaller size
 # X and Y positions for movement later
 enemyImgX = random.randint(0, 1400)
 enemyImgY = random.randint(0, 1000)
+enemyScale = 10
 enemyX_change = 0
 enemyY_change = 0
-enemyScale = 10
 enemy_shot_status = False
 
 
@@ -57,16 +57,17 @@ while running:
         if not enemy_shot_status and 1000 >= enemyImgY >= 0 and 1400 >= enemyImgX >= 0:
             enemyImgY += random.randint(-5, 5)
             enemyImgX += random.randint(-5, 5)
-            enemyImg = pygame.transform.scale(enemyImg, (random.randint(1, 5), random.randint(1, 5)))  # Scale it slowly
         elif not enemy_shot_status and not 1000 >= enemyImgY >= 0 or not 1400 >= enemyImgX >= 0:
             enemyImgY += random.randint(5, -5)
             enemyImgX += random.randint(5, -5)
-            enemyImg = pygame.transform.scale(enemyImg, (random.randint(1, 5), random.randint(1, 5)))  # Scale it slowly
         elif enemy_shot_status:
             enemy.kill()
 
+        if not enemy_shot_status:
+            enemyImg = pygame.transform.scale(enemyImg, (55, 20))  # Update scale
+
     # Game refreshing
     enemy(enemyImg, enemyImgX, enemyImgY)
-    ship(shipImgPos[0] / 75 - 20, shipImgPos[1] / 75 - 20)
+    ship(shipImgPos[0] / 62 - 20, shipImgPos[1] / 62 - 20)
 
     pygame.display.update()
